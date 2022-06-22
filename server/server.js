637d63de4461332b8cd92772915dab641dee2c61
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-06-21 18:54:09 -0600

    fixes

--- server/server.js
+++ server/server.js
@@ -37,16 +37,12 @@ const startApolloServer = async (typeDefs, resolvers) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
-  server.listen().then(({ url }) => {
-    12
-      console.log(`  Server ready at ${url}`);
-    13
-    });
-    // app.listen(PORT, () => {
-    //   console.log(`API server running on port ${PORT}!`);
-    //   // log where we can go to test our GQL API
-    //   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    // })
+ 
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
  
 };
 
